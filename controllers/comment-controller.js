51e1f2afe6da20d2d0ab62b8a58376c396622252
NJShanta <njshanta.ca@gmail.com>
2024-12-01 10:11:43 -0500

    Add delete comment queries

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -9,6 +9,7 @@ const index = async (req, res) => {
     let comments = await knex("comments")
       .select(
         "comments.id as comment_id",
+        "comments.game_id",
         "comments.user_id",
         "comments.message",
         "comments.created_at",
@@ -36,6 +37,7 @@ const findOne = async (req, res) => {
     let comment = await knex("comments")
       .select(
         "comments.id as comment_id",
+        "comments.game_id",
         "comments.user_id",
         "comments.message",
         "comments.created_at",
@@ -56,4 +58,69 @@ const findOne = async (req, res) => {
     res.status(500).json({ message: "Unable to retrive comments data" });
   }
 };
-export { index, findOne };
+
+const add = async (req, res) => {
+  try {
+    let { game_id, user_id, message } = req.body;
+
+    const parsedUserId = parseInt(user_id, 10);
+
+    if (isNaN(parsedUserId)) {
+      return res.status(400).json({
+        message: "'user_id' must be a valid number",
+      });
+    }
+    user_id = parsedUserId;
+    const parsedGameId = parseInt(game_id, 10);
+    if (isNaN(parsedGameId)) {
+      return res.status(400).json({
+        message: "'game_id' must be a valid number",
+      });
+    }
+    game_id = parsedGameId;
+
+    if (message?.trim() == "") {
+      return res.status(400).json({
+        message: "message can't be empty or have whitespace",
+      });
+    }
+    const addedComment = await knex("comments").insert({
+      game_id,
+      user_id,
+      message,
+    });
+
+    const id = addedComment[0];
+    const { updated_at, ...newComment } = await knex("comments")
+      .where({ id })
+      .first();
+    res.status(201).json(newComment);
+    if (newComment) {
+      newComment.avatar_path = getAvatarPath(comment.avatar_path);
+      res.status(200).json(newComment);
+    } else {
+      res.status(404).json({ message: `Comment with ID ${id} doesn't exist` });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Unable to retrive comments data" });
+  }
+};
+
+const remove = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const selectedComment = await knex("comments").where({ id });
+
+    if (selectedComment.length > 0) {
+      await knex("comments").where({ id }).del();
+      res.sendStatus(204);
+    } else {
+      return res.status(404).json({ message: "Comment does not exist" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error });
+  }
+};
+export { index, findOne, add, remove };
--- routes/comment-routes.js
+++ routes/comment-routes.js
@@ -3,6 +3,9 @@ import * as commentController from "../controllers/comment-controller.js";
 
 const router = express.Router();
 
-router.route("/").get(commentController.index);
-router.route("/:id").get(commentController.findOne);
+router.route("/").get(commentController.index).post(commentController.add);
+router
+  .route("/:id")
+  .get(commentController.findOne)
+  .delete(commentController.remove);
 export default router;
