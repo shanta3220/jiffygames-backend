3f110ba9e3f616fcc74f86e204a8e9d1ed84da22
NJShanta <njshanta.ca@gmail.com>
2024-12-03 21:23:17 -0500

    change index.js

--- index.js
+++ index.js
@@ -11,27 +11,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-// Serve gzipped Unity files with correct headers
-app.get("*.js.gz", (req, res, next) => {
-  res.set("Content-Encoding", "gzip"); // Inform the browser the file is gzipped
-  res.set("Content-Type", "application/javascript"); // Set the JavaScript MIME type
-  next();
-});
-
-app.get("*.data.gz", (req, res, next) => {
-  res.set("Content-Encoding", "gzip"); // Inform the browser the file is gzipped
-  res.set("Content-Type", "application/octet-stream"); // Correct MIME type for .data files
-  next();
-});
-
-app.get("*.wasm.gz", (req, res, next) => {
-  res.set("Content-Encoding", "gzip"); // Inform the browser the file is gzipped
-  res.set("Content-Type", "application/wasm"); // Correct MIME type for WebAssembly
-  next();
-});
-
 const PORT = process.env.PORT || 5050;
-
+const BACKEND_URL = process.env.BACKEND_URL || localhost;
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to GiffyGamesApi" });
 });
@@ -42,5 +23,5 @@ app.use("/leaderboards", leaderboardRoutes);
 app.use("/comments", commentRoutes);
 
 app.listen(PORT, () => {
-  console.log(`running at http://localhost:${PORT}`);
+  console.log(`running at http://${BACKEND_URL}:${PORT}`);
 });
